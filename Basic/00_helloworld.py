4f84067e5b37fb69670c2ff4b9318f05d1a9d805
mariajaramillo7 <mariajara96@gmail.com>
2026-07-29 17:46:29 -0500

    Add data type examples in 00_helloworld.py
    Added examples of various data types in Python, including str, int, float, complex, bool, set, dict, tuple, range, list, and NoneType.

--- Basic/00_helloworld.py
+++ Basic/00_helloworld.py
@@ -32,10 +32,23 @@ en varias líneas
 # comentario
 # en varias líneas
 
+# Tipos de datos 
+'Hello World' # Tipo str'. Secuencia de caracteres envuelto por comillas.
+5 # Tipo 'int'. Número entero
+1.5 # Tipo 'float'. Números con decimales
+3 + 1j  # Tipo 'complex'
+True  # Tipo 'bool'. Valor tipo TRUE or FALSE.
+{7, 'hello', 8.5} # Set. Colección no ordenada de elementos únicos
+{'name': 'Alice', 'age': 25} # Tipo 'dict'. Dictionary. Información en pares de clave y valor, envueltos en llaves {}
+(7, 'hello', 8.5) # Tipo 'tuple'. Tuple (Tupla). Colección ordenada e inmutable de elementos. A diferencia de las listas, sus valores NO se pueden modificar, añadir ni eliminar. Se encierran en paréntesis. 
+range(5) # Range. Secuencia de números, usado comúnmente en loops.
+[22, 'Hello world', 3.14, True] # List. Lista ordenada de elementos, soporta distintos tipos de datos.
+None # Tipo 'NoneType'. None. Representa ausencia de valor
+
 # Cómo consultar el tipo de dato
-print(type("Soy un dato str"))  # Tipo 'str'
-print(type(5))  # Tipo 'int'
-print(type(1.5))  # Tipo 'float'
-print(type(3 + 1j))  # Tipo 'complex'
-print(type(True))  # Tipo 'bool'
-print(type(print("Mi cadena de texto")))  # Tipo 'NoneType' → print es una función del sistema que realiza una acción, no devuelve ningún valor (su valor de retorno es None)
+print(type('str'))
+print(type(5))
+print(type(nombre_variable))
+
+
+
