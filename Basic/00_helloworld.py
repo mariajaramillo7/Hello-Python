387e42d825a0c177ecce9b50a7b23cac633609df
mariajaramillo7 <mariajara96@gmail.com>
2026-07-28 22:03:25 -0500

    Enhance hello world example with print statements
    Added print statements and comments to demonstrate output and data types.

--- Basic/00_helloworld.py
+++ Basic/00_helloworld.py
@@ -2,11 +2,19 @@
 
 ### Hola Mundo ###
 
-# Nuestro hola mundo en Python
-print("Hola Python")
-print('Hola Python')
 
+# Print Function
+Mostrar información en la pantalla
+print('Hello world!')
+
+print('My favorite colors are', 'blue', 'green', 'red')  # Output: My favorite colors are blue green red
+# Python agrega automáticamente un espacio entre cada item cuando los separas con comas 
+
+--- Comentarios: para explicar tu código, dejarte recordatorios o aclarar por qué existe una línea.
+NO deberían usarse para explicar nombres de variables, estos ya deben ser descriptivos.
+  
 # Esto es un comentario
+x = 56 # Esto también es un comentario → Python ignora todo después del # en esa línea
 
 """
 Este es un
@@ -19,6 +27,10 @@ Este también es un
 comentario
 en varias líneas
 '''
+
+# Este también es un 
+# comentario
+# en varias líneas
 
 # Cómo consultar el tipo de dato
 print(type("Soy un dato str"))  # Tipo 'str'
