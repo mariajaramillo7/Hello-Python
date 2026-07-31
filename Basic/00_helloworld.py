9639eb342ba1859a7c7076b532e99edb717e221a
mariajaramillo7 <mariajara96@gmail.com>
2026-07-31 18:48:37 -0500

    Remove data type comments from helloworld.py
    Removed comments explaining various data types in Python.

--- Basic/00_helloworld.py
+++ Basic/00_helloworld.py
@@ -32,18 +32,6 @@ en varias líneas
 # comentario
 # en varias líneas
 
-# Tipos de datos 
-'Hello World' # Tipo str'. Secuencia de caracteres envuelto por comillas.
-5 # Tipo 'int'. Número entero
-1.5 # Tipo 'float'. Números con decimales
-3 + 1j  # Tipo 'complex'. Números complejos - con números reales e imaginarios.
-True  # Tipo 'bool'. Valor tipo TRUE or FALSE. En general se usan para tomar decisiones en tu código.
-{7, 'hello', 8.5} # Set. Colección no ordenada de elementos únicos
-{'name': 'Alice', 'age': 25} # Tipo 'dict'. Dictionary. Información en pares de clave y valor, envueltos en llaves {}
-(7, 'hello', 8.5) # Tipo 'tuple'. Tuple (Tupla). Colección ordenada e inmutable de elementos. A diferencia de las listas, sus valores NO se pueden modificar, añadir ni eliminar. Se encierran en paréntesis. 
-range(5) # Range. Secuencia de números, usado comúnmente en loops.
-[22, 'Hello world', 3.14, True] # List. Lista ordenada de elementos, soporta distintos tipos de datos.
-None # Tipo 'NoneType'. None. Representa ausencia de valor
 
 # Cómo consultar el tipo de dato
 - Type
